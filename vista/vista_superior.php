2ef09d75cb4ed227ea9acd4d9cef4c7bdddccef1
Mhya948 <bs.mhya.20@gmail.com>
2025-10-28 18:39:47 -0600

    Creacion de Proyecto

---
+++ vista/vista_superior.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="Spanish">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="tercera 1" content="Proyecto Universitario">
+ <title>Tercera 1</title>
+ <link rel="stylesheet" href=./CSS/main.css>
+
+
+
+</head>
+
+
